6f0e6312d99f43198bd13eb85e1ba3570636cfc7
NguyenTanNghi <nguyentannghi5722@gmail.com>
2026-01-22 13:10:17 +0700

    Cập nhật cấu hình Mongoose và thêm các dependencies cần thiết cho dự án

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [MongooseModule.forRoot('mongodb+srv://nguyentannghi5722_db_user:3OZ6Wtkoi9auQzJe@cluster0.aw8bcwb.mongodb.net/')],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
