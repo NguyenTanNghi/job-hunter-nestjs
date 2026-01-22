66fb4ab62df69d479ce59fdcd2217b91e93e695e
NguyenTanNghi <nguyentannghi5722@gmail.com>
2026-01-22 17:30:50 +0700

    Thêm ValidationPipe toàn cục và cập nhật CreateUserDto với các quy tắc xác thực email

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,6 +1,14 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
 export class CreateUserDto {
+    @IsEmail({}, { message: 'Email không hợp lệ' })
+    @IsNotEmpty({ message: 'Email không được để trống' })
     email: string;
+
+    @IsNotEmpty({ message: 'Mật khẩu không được để trống' })
     password: string;
+
     name: string;
+
     address: string;
 }
