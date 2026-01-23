5dd89bddf052b1c01f5c37fb5d4d548d3cb79c96
NguyenTanNghi <nguyentannghi5722@gmail.com>
2026-01-23 10:34:55 +0700

    Thêm LocalAuthGuard và cập nhật AppController để sử dụng chiến lược xác thực 'local' cho route đăng nhập

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,18 +1,25 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Post, Render, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
     constructor(
         private readonly appService: AppService,
-         private configService: ConfigService // sử dụng ConfigService trong AppController
-        ) { }
+        private configService: ConfigService // sử dụng ConfigService trong AppController
+    ) { }
 
-    @Get()//=> restful API
-    @Render("home") //=> SSR
-    handleHomePage() {
-        const message = this.appService.getHello();
-        return { message };
+    // @Get()//=> restful API
+    // @Render("home") //=> SSR
+    // handleHomePage() {
+    //     const message = this.appService.getHello();
+    //     return { message };
+    // }
+
+    @UseGuards(LocalAuthGuard) // Sử dụng AuthGuard với chiến lược 'local' để bảo vệ route này
+    @Post('/login')
+    handleLogin(@Request() req) {
+        return req.user; // Trả về thông tin người dùng đã được xác thực
     }
 }
