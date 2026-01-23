5cae01bf4bc3071edea3c6cfae94a7f8453c2124
NguyenTanNghi <nguyentannghi5722@gmail.com>
2026-01-23 11:09:36 +0700

    Cập nhật AppController và AuthService để trả về JWT token sau khi đăng nhập thành công; thêm cấu hình JwtModule vào AuthModule

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Get, Post, Render, Request, UseGuards } from '@nestjs/commo
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
     constructor(
         private readonly appService: AppService,
-        private configService: ConfigService // sử dụng ConfigService trong AppController
+        private configService: ConfigService, // sử dụng ConfigService trong AppController
+        private authService: AuthService
     ) { }
 
     // @Get()//=> restful API
@@ -20,6 +22,6 @@ export class AppController {
     @UseGuards(LocalAuthGuard) // Sử dụng AuthGuard với chiến lược 'local' để bảo vệ route này
     @Post('/login')
     handleLogin(@Request() req) {
-        return req.user; // Trả về thông tin người dùng đã được xác thực
+        return this.authService.login(req.user);// Trả về JWT token sau khi đăng nhập thành công
     }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) { }
+    constructor(private usersService: UsersService, private jwtService: JwtService) { }
 
     // Hàm này dùng để xác thực người dùng khi họ đăng nhập, username và password là 2 tham số của thư viện passport truyền vào
     async validateUser(username: string, password: string): Promise<any> {
@@ -15,5 +16,13 @@ export class AuthService {
             }
         }
         return null;
+    }
+
+    // Hàm này dùng để tạo và trả về JWT token khi người dùng đăng nhập thành công
+    async login(user: any) {
+        const payload = { username: user.email, sub: user._id };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
     }
 }
