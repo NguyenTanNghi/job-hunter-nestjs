ee51b06c772dbcd44f061efbd67b83847187acca
NguyenTanNghi <nguyentannghi5722@gmail.com>
2026-01-24 09:29:33 +0700

    Cập nhật cấu hình JWT để sử dụng 'JWT_ACCESS_TOKEN' và thêm gói 'ms' để quản lý thời gian hết hạn token

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { LocalStrategy } from './passport/local.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './passport/jwt.strategy';
+import ms from 'ms';
 
 @Module({
     imports: [
@@ -15,9 +16,9 @@ import { JwtStrategy } from './passport/jwt.strategy';
         JwtModule.registerAsync({
             imports: [ConfigModule],
             useFactory: async (configService: ConfigService) => ({
-                secret: configService.get<string>('JWT_SECRET'),
+                secret: configService.get<string>('JWT_ACCESS_TOKEN'),
                 signOptions: {
-                    expiresIn: configService.get<string>('JWT_EXPIRE')
+                    expiresIn: ms(configService.get<string>('JWT_ACCESS_EXPIRE')) // chuyển đổi thời gian từ chuỗi sang số giây
                 },
             }),
             inject: [ConfigService],
