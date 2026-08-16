f9e196c635fef5d1c7c8bd9a708bb4db37eb8636
NguyenTanNghi <nguyentannghi5722@gmail.com>
2026-08-16 22:43:53 +0700

    feat: add files module controller and postman API collection for testing

--- backend/src/files/files.controller.ts
+++ backend/src/files/files.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, HttpStatus, ParseFilePipeBuilder, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FilesService } from './files.service';
+import { ResponseMessage, Public } from 'src/auth/decorator/customize';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) { }
 
   @Post('upload')
+  @ResponseMessage('Tải lên một tệp tin')
   @UseInterceptors(FileInterceptor('file')) //tên field sử dụng trong form-data
   uploadFile(@UploadedFile(
     new ParseFilePipeBuilder()
@@ -14,11 +16,13 @@ export class FilesController {
         fileType: /^(jpg|jpeg|png|image\/png|gif|txt|pdf|doc|docx|text\/plain)$/i,
       })
       .addMaxSizeValidator({
-        maxSize: 1000
+        maxSize: 1024 * 1024 // 1MB
       })
       .build({
         errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY
       })) file: Express.Multer.File) {
-    console.log(file);
+    return {
+      fileName: file.filename
+    };
   }
 }
