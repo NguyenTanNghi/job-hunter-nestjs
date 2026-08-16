4368408a19f7cf35771058210a58bd0bacc40905
NguyenTanNghi <nguyentannghi5722@gmail.com>
2026-08-16 13:27:11 +0700

    feat: add User schema and create-user DTO for user module

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,6 +1,18 @@
-import { IsEmail, IsNotEmpty } from "class-validator";
+import { IsEmail, IsNotEmpty, IsNotEmptyObject, IsObject, ValidateNested } from "class-validator";
+import { Type } from "class-transformer";
+import mongoose from "mongoose";
 
+class Company {
+  @IsNotEmpty({ message: 'ID Company không được để trống' })
+  _id: mongoose.Schema.Types.ObjectId;
+
+  @IsNotEmpty({ message: 'Tên công ty không được để trống' })
+  name: string;
+}
 export class CreateUserDto {
+  @IsNotEmpty({ message: 'Tên không được để trống' })
+  name: string;
+
   @IsEmail({}, { message: 'Email không hợp lệ' })
   @IsNotEmpty({ message: 'Email không được để trống' })
   email: string;
@@ -8,7 +20,42 @@ export class CreateUserDto {
   @IsNotEmpty({ message: 'Mật khẩu không được để trống' })
   password: string;
 
+  @IsNotEmpty({ message: 'Tuổi không được để trống' })
+  age: number
+
+  @IsNotEmpty({ message: 'Giới tính không được để trống' })
+  gender: string
+
+  @IsNotEmpty({ message: 'Địa chỉ không được để trống' })
+  address: string;
+
+  @IsNotEmpty({ message: 'Role không được để trống' })
+  role: string;
+
+  @IsNotEmptyObject()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Company)
+  company: Company;
+}
+
+export class RegisterUserDto {
+  @IsNotEmpty({ message: 'Tên không được để trống' })
   name: string;
 
+  @IsEmail({}, { message: 'Email không hợp lệ' })
+  @IsNotEmpty({ message: 'Email không được để trống' })
+  email: string;
+
+  @IsNotEmpty({ message: 'Mật khẩu không được để trống' })
+  password: string;
+
+  @IsNotEmpty({ message: 'Tuổi không được để trống' })
+  age: number
+
+  @IsNotEmpty({ message: 'Giới tính không được để trống' })
+  gender: string
+
+  @IsNotEmpty({ message: 'Địa chỉ không được để trống' })
   address: string;
 }
