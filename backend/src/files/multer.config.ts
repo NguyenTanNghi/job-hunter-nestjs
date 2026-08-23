7bc009dc79019fd8dd987d80171ced96c0d9d913
NguyenTanNghi <nguyentannghi5722@gmail.com>
2026-08-23 09:31:26 +0700

    feat: add TypeScript configuration and Multer file upload service for storage management

--- backend/src/files/multer.config.ts
+++ backend/src/files/multer.config.ts
@@ -15,7 +15,6 @@ export class MulterConfigService implements MulterOptionsFactory {
   ensureExists(targetDirectory: string) {
     fs.mkdir(targetDirectory, { recursive: true }, (error) => {
       if (!error) {
-        console.log('Directory successfully created, or it already exists.');
         return;
       }
       switch (error.code) {
@@ -37,28 +36,31 @@ export class MulterConfigService implements MulterOptionsFactory {
         destination: (req, file, cb) => {
           const folder = req?.headers?.folder_type ?? "default";
           this.ensureExists(`public/images/${folder}`);
-          cb(null, join(this.getRootPath(), `public/images/${folder}`))
+          cb(null, join(this.getRootPath(), `public/images/${folder}`));
         },
         filename: (req, file, cb) => {
+          // get image extension
           let extName = path.extname(file.originalname);
+          // get image's name (without extension)
           let baseName = path.basename(file.originalname, extName);
-          let finalName = `${baseName}-${Date.now()}${extName}`
-          cb(null, finalName)
-        }
+
+          let finalName = `${baseName}-${Date.now()}${extName}`;
+          cb(null, finalName);
+        },
       }),
       fileFilter: (req, file, cb) => {
-        const allowedFileTypes = /^(jpg|jpeg|png|gif|txt|pdf|doc|docx)$/i;
-        const isValidExtname = allowedFileTypes.test(path.extname(file.originalname).replace('.', ''));
-        const isValidMimetype = allowedFileTypes.test(file.mimetype);
+        const allowedFileTypes = ['jpg', 'jpeg', 'png', 'gif', 'pdf', 'doc', 'docx'];
+        const fileExtension = file.originalname.split('.').pop().toLowerCase();
+        const isValidFileType = allowedFileTypes.includes(fileExtension);
 
-        if (isValidExtname || isValidMimetype) {
-          return cb(null, true);
+        if (!isValidFileType) {
+          cb(new HttpException('Invalid file type', HttpStatus.UNPROCESSABLE_ENTITY), null);
         } else {
-          cb(new HttpException(`File không đúng định dạng. Chỉ chấp nhận các định dạng: jpg, jpeg, png, gif, txt, pdf, doc, docx`, HttpStatus.UNPROCESSABLE_ENTITY), false);
+          cb(null, true);
         }
       },
       limits: {
-        fileSize: 1024 * 1024 * 5 // 5MB limit
+        fileSize: 1024 * 1024 * 1 // 1MB
       }
     };
   }
